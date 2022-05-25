557eabe96988dab80faf940bbbcb9a6b5a265a4a
Shaha-Alam <shahaalam3002@gmail.com>
2022-05-25 21:09:13 +0600

    Reviews-hooks-app.css-app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../src/components/Home/Home';
+import Header from '../src/components/Header/Header';
+import Reviews from '../src/components/Reviews/Reviews';
+import NotFound from './components/NotFound/NotFound';
+import Dashboard from './components/Dashboard/Dashboard';
+import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
+import HomeReview from './components/HomeReview/HomeReview';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<><Home></Home><HomeReview></HomeReview></>}></Route>
+        <Route path='/home' element={<><Home></Home><HomeReview></HomeReview></>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
